6be895a19cb782380f8a92b4f0299961153149c8
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2023-01-10 10:14:24 -0500

    minor tests

--- EEG Game/Assets/Scripts/MouseLook.cs
+++ EEG Game/Assets/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] float sensetivityX = 8f;
-    [SerializeField] float sensetivityY = 0.5f;
+    [SerializeField] float sensetivityY = 8f;
     float mouseX, mouseY;
 
 
