22b5292d70b61ae1e07c340b07350ff25eb6a5a1
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-02-19 14:33:40 -0500

    Working teleport function

--- EEG Game/Assets/Scripts/EegBenchmark.cs
+++ EEG Game/Assets/Scripts/EegBenchmark.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EegBenchmark : MonoBehaviour
 {
+    private Camera m_MainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        m_MainCamera = Camera.main;
         createCube(0, 0, 0, 10);
         createCube(0, 0, 1, 5);
         createCube(0, 0, 0, 1);
@@ -21,9 +24,15 @@ public class EegBenchmark : MonoBehaviour
 
     }
 
+    /**
+     * Teleports the player to given coordinates.
+     *
+     * ax, ay, and az are all in degrees, as if you were entering them in the Unity editor
+     */
     public void teleportPlayer(float x, float y, float z, float ax, float ay, float az)
     {
-
+        m_MainCamera.transform.position = new Vector3(x, y, z);
+        m_MainCamera.transform.eulerAngles = new Vector3(ax, ay, az);
     }
 
     public void createCube(float x, float y, float z, float L)
