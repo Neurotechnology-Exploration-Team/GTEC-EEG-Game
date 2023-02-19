9cc7eee8367951ae5a20a1b86f276fca400c5a7d
DanielT36 <48529242+DanielT36@users.noreply.github.com>
2023-02-19 16:08:49 -0500

    Update EegBenchmark.cs

--- EEG Game/Assets/Scripts/EegBenchmark.cs
+++ EEG Game/Assets/Scripts/EegBenchmark.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
+using System;
 
 public class EegBenchmark : MonoBehaviour
 {
@@ -10,12 +12,13 @@ public class EegBenchmark : MonoBehaviour
     void Start()
     {
         m_MainCamera = Camera.main;
-        createCube(0, 0, 0, 10);
+        /*createCube(0, 0, 0, 10);
         createCube(0, 0, 1, 5);
         createCube(0, 0, 0, 1);
         createCube(0, 0, 10, 5);
         destroyCube(0, 0, 0);
-        destroyAllCubes();
+        destroyAllCubes();*/
+        StartCoroutine(distanceTest(3000));
     }
 
     // Update is called once per frame
@@ -44,17 +47,17 @@ public class EegBenchmark : MonoBehaviour
     public void destroyCube(float x, float y, float z)
     {
         GameObject[] gameObjects = FindObjectsOfType(typeof(GameObject)) as GameObject[];
-       
-        foreach(GameObject gameObject in gameObjects)
+
+        foreach (GameObject gameObject in gameObjects)
         {
-       
+
             if (gameObject.name == "TestingCube" && gameObject.transform.position == new Vector3(x, y, z))
             {
                 Destroy(gameObject, 5);
             }
-                       
+
         }
-           
+
     }
 
     public void destroyAllCubes()
@@ -71,6 +74,33 @@ public class EegBenchmark : MonoBehaviour
 
         }
 
+    }
+
+    public IEnumerator distanceTest(float time_change)
+    {
+        System.Random rng = new System.Random();
+
+        createCube(0, 0, 0, 1);
+        var dist = 1;
+        teleportPlayer(dist, 0, 0, 0, 270, 0);
+        int certainty = rng.Next(10);
+        while (true)
+        {
+            DateTime start = DateTime.UtcNow;
+            long startTimeMilliseconds = new DateTimeOffset(start).ToUnixTimeMilliseconds();
+            teleportPlayer(dist*=2, 0, 0, 0, 270, 0);
+            while (true)
+            {
+                UnityEngine.Debug.LogFormat("{0}, {1}", dist, certainty);
+                DateTime now = DateTime.UtcNow;
+                long unixTimeMilliseconds = new DateTimeOffset(now).ToUnixTimeMilliseconds();
+                yield return new WaitForSeconds(.1f);
+                if (unixTimeMilliseconds - startTimeMilliseconds > time_change)
+                {
+                    break;
+                }
+            }
+        }
     }
 }
 
